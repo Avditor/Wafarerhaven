8bdcf36d122f4e78cd16d0b4ecc48434a9e61441
Avni <avnii.k02@gmail.com>
2025-01-26 23:31:34 +0530

    cloudConnect

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wf";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wf";
 
 main()
   .then(() => {
@@ -13,7 +13,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
